059cc9f7528ba4a778e85d761db73f7fdb4fd61b
Pramit Choudhary <pramitchoudhary@gmail.com>
2018-05-26 17:37:57 -0700

    resolving flake8 related issue

--- skater/core/global_interpretation/interpretable_models/brlc.py
+++ skater/core/global_interpretation/interpretable_models/brlc.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from skater.util import exceptions
+
 import numbers
 import numpy as np
 import pandas as pd
@@ -7,7 +9,6 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 pandas2ri.activate()
 
-from skater.util import exceptions
 
 class BRLC(object):
     """ :: Experimental :: The implementation is currently experimental and might change in future
