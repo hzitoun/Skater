bd51d2e09fed56282057ac4972e9f9c6b870de8d
Pramit Choudhary <pramitchoudhary@gmail.com>
2018-05-29 16:33:23 -0700

    resolving missedout flake8 indendation error

--- skater/tests/test_dnni.py
+++ skater/tests/test_dnni.py
@@ -98,7 +98,7 @@ class TestDNNI(unittest.TestCase):
             # test for optional arguments related to window_size and step size for 'occlusion'
             optional_args = {"window_size": 2, "step": 2}
             relevance_scores_occ = di.explain('occlusion', target_tensor * ys, input_tensor, xs,
-                                                use_case='image', **optional_args)
+                                              use_case='image', **optional_args)
         self.assertEquals(relevance_scores_elrp.shape, (2, 28, 28, 1))
         self.assertEquals(relevance_scores_ig.shape, (2, 28, 28, 1))
         self.assertEquals(relevance_scores_occ.shape, (2, 28, 28, 1))
