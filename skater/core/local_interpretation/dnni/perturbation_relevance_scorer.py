145edc0d26bcd21ce29a179f4f1e013cf5d872fc
Pramit Choudhary <pramitchoudhary@gmail.com>
2018-05-29 13:30:33 -0700

    enabled ability to customize window_size and step_size for perturbing feature space with occlusion; improved unit tests

--- skater/core/local_interpretation/dnni/perturbation_relevance_scorer.py
+++ skater/core/local_interpretation/dnni/perturbation_relevance_scorer.py
@@ -46,17 +46,18 @@ class Occlusion(BasePerturbationMethod):
     __name__ = "Occlusion"
     logger = build_logger(_INFO, __name__)
 
-    def __init__(self, output_tensor, input_tensor, samples, current_session, **kargs):
+    def __init__(self, output_tensor, input_tensor, samples, current_session, **kwargs):
         super(Occlusion, self).__init__(output_tensor, input_tensor, samples, current_session)
 
         self.input_shape = samples.shape[1:]
-        self.replace_value = kargs if 'replace_value' in kargs.keys() else 0
-        self.window_size = kargs if 'window_size' in kargs.keys() else 2
+        self.replace_value = kwargs['replace_value'] if 'replace_value' in kwargs.keys() else 0
+        self.window_size = kwargs['window_size'] if 'window_size' in kwargs.keys() else 1
+        self.step = kwargs['step'] if 'step' in kwargs.keys() else 1
         # the input samples are expected to be of the shape,
         # (1, 150, 150, 3) <batch_size, image_width, image_height, no_of_channels>
         self.batch_size = self.samples.shape[0]
-        Occlusion.logger.info('Input shape: {}; window_size: {}; replace value: {}; batch size: {}'.
-                              format(self.input_shape, self.window_size, self.replace_value, self.batch_size))
+        Occlusion.logger.info('Input shape: {}; window_size/step: ({}/{}); replace value: {}; batch size: {}'.
+                              format(self.input_shape, self.window_size, self.step, self.replace_value, self.batch_size))
 
 
     def _create_masked_input(self, row_value, col_value):
@@ -72,7 +73,7 @@ class Occlusion(BasePerturbationMethod):
         Occlusion.logger.info('Shape of the mask patch: {}'.format(mask.shape))
         relevance_score = np.zeros_like(self.samples, dtype=np.float32)
         # normalizer matrix is set to 1 default; as matrix cell gets used atleast once
-        normalizer = np.ones_like(relevance_score)
+        normalizer = np.zeros_like(relevance_score)
 
         # Compute original output
         default_eval = self._session_run(self.output_tensor, self.samples)
@@ -80,8 +81,8 @@ class Occlusion(BasePerturbationMethod):
 
         count = 1  # to keep track of the number of times a matrix cell is used while perturbing through the feature space
         # Perturb through the feature space by replacing and masking
-        for row in range(self.samples[0].shape[0] - self.window_size):
-            for col in range(self.samples[0].shape[1] - self.window_size):
+        for row in range(0, self.samples[0].shape[0] - self.window_size, self.step):
+            for col in range(0, self.samples[0].shape[1] - self.window_size, self.step):
                 # create masked input while rolling through the input matrix
                 new_input = self._create_masked_input(row, col)
                 # compute entropy when compared to the original eval value
@@ -93,7 +94,8 @@ class Occlusion(BasePerturbationMethod):
                 # on window size
                 normalizer[:, row:row + self.window_size, col:col + self.window_size, :] += count
 
-        Occlusion.logger.info("Maximum normalizer weight: {}".format(np.max(normalizer.shape)))
+        Occlusion.logger.info("Min/Max normalizer weight: {}".format(np.min(normalizer.shape),
+                                                                     np.max(normalizer.shape)))
         relevance_score_norm = relevance_score / normalizer
         Occlusion.logger.info("relevance score matrix shape :{}".format(relevance_score_norm.shape))
         return relevance_score_norm
