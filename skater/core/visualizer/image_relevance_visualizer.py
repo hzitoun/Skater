7a242904e52493019faa5eb88668212d812eac11
Pramit Choudhary <pramitchoudhary@gmail.com>
2018-05-31 10:48:49 -0700

    code improvement, handling exceptions for third party imports(matplotlib)

--- skater/core/visualizer/image_relevance_visualizer.py
+++ skater/core/visualizer/image_relevance_visualizer.py
@@ -2,9 +2,16 @@
 
 from skimage.filters import roberts, sobel
 import numpy as np
-import matplotlib.pyplot as plt
+from skater.util.exceptions import MatplotlibUnavailableError
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    raise (MatplotlibUnavailableError("Matplotlib is required but unavailable on your system."))
 
 from skater.util.image_ops import normalize
+
+# helper function to enable or disable matplotlib access
+_enable_axis = lambda ax, flag: ax.axis("off") if flag is True else ax.axis("on")
 
 
 def visualize(relevance_score, original_input_img=None, edge_detector_type='sobel', cmap='bwr', axis=plt,
@@ -32,7 +39,7 @@ def visualize(relevance_score, original_input_img=None, edge_detector_type='sobe
     # Plot the image with relevance scores
     axis.imshow(relevance_score, extent=extent, interpolation='nearest', cmap=cmap,
                 vmin=-abs_min, vmax=abs_max, alpha=alpha_bgcolor)
-    axis.axis('off') if disable_axis is True else axis.axis('on')
+    _enable_axis(disable_axis)
     return axis
 
 
